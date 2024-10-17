8b9b7f1eb9ab062f0cdbe367b53b23f084c29cac
Carlos Fernández <samuelsfer.06@gmail.com>
2024-10-17 10:30:42 -0600

    Create config.js

---
+++ todo-list-app/src/firebase/config.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+//firestore
+import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+   apiKey: "AIzaSyDndINLuToIC11o5FuCRqtnK2V2rxp7obw",
+   authDomain: "auth-app-1998e.firebaseapp.com",
+   projectId: "auth-app-1998e",
+   storageBucket: "auth-app-1998e.appspot.com",
+   messagingSenderId: "188423563383",
+   appId: "1:188423563383:web:deeff6d481845655e0fe72",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Inicializamos firestore
+export const db = getFirestore(app);
